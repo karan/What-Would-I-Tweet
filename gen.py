67cbe108f71144ed24154977200c60eba8da5fb4
Karan Goel <karan@goel.im>
2013-11-18 16:38:46 -0800

    Save auth tokens

--- gen.py
+++ gen.py
@@ -11,31 +11,51 @@ import operator
 from twython import Twython
 
 
+TOKEN_FILE = 'tokens.cfg'
+
+def get_tokens():
+    config = ConfigParser.RawConfigParser()
+    config.read(TOKEN_FILE)
+    return config.get('OAUTH', 'OAUTH_TOKEN'), config.get('OAUTH', 'OAUTH_TOKEN_SECRET')
+
 #-- Twitter Oauth --#
-# get twitter api keys
-config = ConfigParser.ConfigParser()
+# get twitter api keys from saved file
+config = ConfigParser.RawConfigParser()
 config.read('settings.cfg')
 app_key = config.get('auth', 'app_key')
 app_secret = config.get('auth', 'app_secret')
+OAUTH_TOKEN, OAUTH_TOKEN_SECRET = '', ''
 
-# Auth
-twitter = Twython(app_key, app_secret)
-auth = twitter.get_authentication_tokens()
-
-OAUTH_TOKEN = auth['oauth_token']
-OAUTH_TOKEN_SECRET = auth['oauth_token_secret']
-
-# send user to auth URL in a new tab
-webbrowser.open(auth['auth_url'], new=2)
-
-# final step of auth - change for web app
-twitter = Twython(app_key, app_secret, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-pin = raw_input('Enter PIN from twitter: ')
-final_step = twitter.get_authorized_tokens(pin)
-
-# TODO: Save these tokens for resuse
-OAUTH_TOKEN = final_step['oauth_token']
-OAUTH_TOKEN_SECRET = final_step['oauth_token_secret']
+try:
+   with open(TOKEN_FILE):
+       OAUTH_TOKEN, OAUTH_TOKEN_SECRET = get_tokens()
+except IOError:
+    # Auth
+    twitter = Twython(app_key, app_secret)
+    auth = twitter.get_authentication_tokens()
+    
+    OAUTH_TOKEN = auth['oauth_token']
+    OAUTH_TOKEN_SECRET = auth['oauth_token_secret']
+    
+    # send user to auth URL in a new tab
+    webbrowser.open(auth['auth_url'], new=2)
+    
+    # final step of auth - change for web app
+    twitter = Twython(app_key, app_secret, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    pin = raw_input('Enter PIN from twitter: ')
+    final_step = twitter.get_authorized_tokens(pin)
+    
+    # TODO: Save these tokens for resuse
+    OAUTH_TOKEN = final_step['oauth_token']
+    OAUTH_TOKEN_SECRET = final_step['oauth_token_secret']
+    
+    # Save tokens
+    config = ConfigParser.RawConfigParser()
+    config.add_section('OAUTH')
+    config.set('OAUTH', 'OAUTH_TOKEN_SECRET', OAUTH_TOKEN_SECRET)
+    config.set('OAUTH', 'OAUTH_TOKEN', OAUTH_TOKEN)
+    with open(TOKEN_FILE, 'wb') as configfile:
+        config.write(configfile)
 
 
 #-- Tweet data --#
