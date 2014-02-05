e159a0c2071289ddc2a9174bfbcbefc07b40940e
Karan Goel <karan@goel.im>
2014-02-04 23:34:59 -0800

    add more styling, sweet button

--- static/app.js
+++ static/app.js
@@ -1,14 +1,19 @@
 var TweetApp = angular.module("TweetApp", []);
 
-TweetApp.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+TweetApp.controller('MainCtrl', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
     $scope.screenName = '';
     $scope.tweets = [];
     $scope.copy = '';
+    $scope.tsrc = '';
+
+    var domain = 'x.goel.im';
+    var base_url = 'https://platform.twitter.com/widgets/tweet_button.html?count=none&size=large&via=' + domain + '&text=';
     
     $scope.getTweet = function (screenName) {
         if ($scope.tweets.length > 0 && screenName == $scope.copy) {
             console.log('already in list');
             $scope.tweet = $scope.tweets.pop();
+            $scope.tsrc = $sce.trustAsResourceUrl(base_url + '"' + $scope.tweet.tweet +'"');
         } else {
             $scope.copy = screenName;
             console.log('not in list');
@@ -20,6 +25,7 @@ TweetApp.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
             .success(function(data, status, headers, config) {
                 $scope.tweets = data['results'];
                 $scope.tweet = $scope.tweets.pop();
+                $scope.tsrc = $sce.trustAsResourceUrl(base_url + '"' + $scope.tweet.tweet +'"');
             })
             .error(function(data, status, headers, config) {
                 // something went wrong!!
